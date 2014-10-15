88ce8ee41c40ae690bf0ee9d3605155621e33922
Luís Puhl <luispuhl@gmail.com>
2014-10-15 03:12:07 -0300

    Melhorado o modelo do banco de dados, adicionado o ticket e as restrições de chave estrangeira; Adicionado diretorio de documentacao (webroot/doc)

---
+++ Festival/webroot/doc/drop.sql
@@ -0,0 +1,17 @@
+
+use festival;
+
+DROP TABLE IF EXISTS `concerts`;
+
+DROP TABLE IF EXISTS `bands`;
+
+DROP TABLE IF EXISTS `stages`;
+
+DROP TABLE IF EXISTS `tickets`;
+
+DROP TABLE IF EXISTS `clients`;
+DROP TABLE IF EXISTS `employees`;
+
+DROP TABLE IF EXISTS `users`;
+
+DROP TABLE IF EXISTS `events`;
